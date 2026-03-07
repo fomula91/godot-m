f074e150ada7bd917ea350f5483860cee2d2540f
jacob <fomula91@gmail.com>
2026-03-07 23:51:13 +0900

    bg 교체 이벤트 버그 수정

--- scenes/main_scene.gd
+++ scenes/main_scene.gd
@@ -35,6 +35,7 @@ var _distraction_free := false
 var _current_bg_id: String = ""
 var _character_slots: Dictionary = {}  # char_id -> slot_name
 var _dialogue_log: Array[Dictionary] = []
+var _bg_tween: Tween
 
 # 거리감 알림 설정
 var _affinity_config: Dictionary = {
@@ -424,11 +425,18 @@ func _on_scene_change(id: String, transition: String) -> void:
 			bg1.texture = tex
 			bg1.modulate.a = 1.0
 		"fadeIn", "fadeFromBlack duration 1500", _:
+			# 이전 트윈이 진행 중이면 즉시 완료 처리
+			if _bg_tween and _bg_tween.is_running():
+				_bg_tween.kill()
+				bg1.texture = bg2.texture
+				bg1.modulate.a = 1.0
+				bg2.modulate.a = 0.0
+
 			bg2.texture = tex
 			bg2.modulate.a = 0.0
-			var tw := create_tween()
-			tw.tween_property(bg2, "modulate:a", 1.0, 1.0)
-			tw.finished.connect(func():
+			_bg_tween = create_tween()
+			_bg_tween.tween_property(bg2, "modulate:a", 1.0, 1.0)
+			_bg_tween.finished.connect(func():
 				bg1.texture = bg2.texture
 				bg1.modulate.a = 1.0
 				bg2.modulate.a = 0.0
